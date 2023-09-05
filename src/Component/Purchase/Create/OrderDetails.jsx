457e02dd6ac377fab5a34fe633252e14c456ac12
moizbroad <mzmjiya@gmail.com>
2023-09-05 14:26:30 -0700

    add Diaog box

--- src/Component/Purchase/Create/OrderDetails.jsx
+++ src/Component/Purchase/Create/OrderDetails.jsx
@@ -3,8 +3,30 @@ import { Container, Box, TextField, Typography, Button, Stack } from '@mui/mater
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import { useState } from 'react';
+import {
+   
+    
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+  } from '@mui/material';
+  import SummaryDetail from '../Summary/SummaryDetail';
 
 const OrderDetails = () => {
+
+
+    const [isChecked, setIsChecked] = useState(false);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+  
+    const handleProceedClick = () => {
+      setIsDialogOpen(true);
+    };
+  
+    const handleCloseDialog = () => {
+      setIsDialogOpen(false);
+    };
     return (
         <Container>
 
@@ -45,9 +67,29 @@ const OrderDetails = () => {
                         <Typography variant="body" gutterBottom marginLeft={1}  >
                             Add Default Shipping Address      </Typography>
 
+                            <div>
+        <Button variant="outlined">Save Draft</Button>
+        <Button variant="contained" onClick={handleProceedClick}>
+          Proceed to Summary
+        </Button>
+      </div>
 
+      {/* Dialog Box */}
+      <Dialog open={isDialogOpen} onClose={handleCloseDialog} maxWidth="lg">
+          <DialogTitle></DialogTitle>
+          <DialogContent>
+            {/* Render the SummaryDetail component when the dialog is open */}
+            {isDialogOpen && <SummaryDetail />}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog} variant="outlined">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
                       
-                    </div>
+                 
 
 
 
--- src/Component/Purchase/Summary/SummaryDetail.jsx
+++ src/Component/Purchase/Summary/SummaryDetail.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Container, Box, Typography, Divider, Select, InputLabel } from '@mui/material'
+import { Container, Box, Typography, Divider, Select, InputLabel, Button } from '@mui/material'
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Checkbox from '@mui/material/Checkbox';
+
+
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -16,37 +18,38 @@ const SummaryDetail = () => {
     };
     return (
         <Container>
-            <Box>
-                <Typography varient='h2' marginTop={10}>
-                    Order Summary
+            <Box sx={{ width: 800, height: 600 }}>
+                <Typography variant="h5" gutterBottom marginBottom={5}>
+                    Summary Details
                 </Typography>
 
                 <Divider />
 
-                <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
-                    <Box>
+                <Box display="flex" alignItems="center" flexDirection="row">
+                    <Box gap={1}>
                         <SupportAgentIcon />
                     </Box>
-                    <Box display="flex" gap={105}>
-                        <p>Supplier</p>
-                        <p >Dog Chew Nation</p>
+                    <Box display="flex" flexGrow={1}> {/* Use flexGrow to take up available width */}
+                        <p style={{ width: '50%', margin: 0 }}>Shipping</p> {/* Adjust width as needed */}
+                        <p style={{ width: '50%', margin: 0 }}>XXX</p> {/* Adjust width as needed */}
                     </Box>
                 </Box>
                 <Divider />
 
-                <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
-                    <Box>
+                <Box display="flex" alignItems="center" flexDirection="row">
+                    <Box gap={1}>
                         <SupportAgentIcon />
                     </Box>
-                    <Box display="flex" gap={105}>
-                        <p>Shipping</p>
-                        <p >XXX</p>
+                    <Box display="flex" flexGrow={1}> {/* Use flexGrow to take up available width */}
+                        <p style={{ width: '50%', margin: 0 }}>Shipping</p> {/* Adjust width as needed */}
+                        <p style={{ width: '50%', margin: 0 }}>XXX</p> {/* Adjust width as needed */}
                     </Box>
                 </Box>
+
                 <Divider />
 
                 <div>
-                    <FormControl variant="standard" sx={{ m: 0.5, minWidth: 1150 }}>
+                    <FormControl variant="standard" sx={{ m: 0.5, width: '100%' }}>
                         <InputLabel id="demo-simple-select-standard-label">Notes</InputLabel>
                         <Select
                             labelId="demo-simple-select-standard-label"
@@ -54,6 +57,7 @@ const SummaryDetail = () => {
                             value={age}
                             onChange={handleChange}
                             label="Age"
+                            sx={{ width: '100%' }}
                         >
                             <MenuItem value="">
                                 <em>None</em>
@@ -65,60 +69,60 @@ const SummaryDetail = () => {
                     </FormControl>
                 </div>
 
+
                 <Box>
-                    <Typography varient='h2' marginTop={10}>
-                        Order Items
+                    <Typography variant="h5" gutterBottom marginBottom={3} marginTop={3}>
+                        Order Item
                     </Typography>
 
                     <Divider />
 
-                    <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
+                    <Box display="flex" alignItems="center" flexDirection="row" gap={0}>
                         <Box>
                             <Checkbox {...label} />
                         </Box>
-                        <Box display="flex" gap={25}>
-                            <p>Supplier</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
+                        <Box sx={{ display: 'flex', gap: '100px', }}>
+                            <p style={{ width: '100%', margin: 0 }}>Supplier</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
                         </Box>
-
-
                     </Box>
                     <Divider />
 
-                    <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
+
+
+                    <Box display="flex" alignItems="center" flexDirection="row" gap={0}>
                         <Box>
                             <Checkbox {...label} />
                         </Box>
-                        <Box display="flex" gap={25}>
-                            <p>Shipping</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
+                        <Box sx={{ display: 'flex', gap: '100px', }}>
+                            <p style={{ width: '100%', margin: 0 }}>Supplier</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
                         </Box>
                     </Box>
                     <Divider />
-                    <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
+
+                    <Box display="flex" alignItems="center" flexDirection="row" gap={0}>
                         <Box>
                             <Checkbox {...label} />
                         </Box>
-                        <Box display="flex" gap={25}>
-                            <p>Shipping</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
-                            <p >Dog Chew Nation</p>
+                        <Box sx={{ display: 'flex', gap: '100px', }}>
+                            <p style={{ width: '100%', margin: 0 }}>Supplier</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
+                            <p style={{ width: '100%', margin: 0 }}>Dog Chew Nation</p> {/* Adjust width as needed */}
                         </Box>
                     </Box>
+
 
 
 
 
                 </Box>
-
+                <Divider />
 
                 <Box>
-                    <Typography varient='h2' marginTop={5}>
+                    <Typography variant="h5" gutterBottom marginBottom={3} marginTop={3}>
                         Cost
                     </Typography>
 
@@ -126,35 +130,40 @@ const SummaryDetail = () => {
                 </Box>
                 <Divider />
 
-                <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
+                <Box display="block" alignItems="center" flexDirection="row" gap={0} marginTop={1}>
 
-                    <Box display="flex" gap={113}>
+                    <Box display="flex" gap={60}>
                         <p>SubTotal</p>
                         <p >$$$$$$$$$   </p>
 
                     </Box>
 
 
-                </Box>
-                <Box display="flex" gap={113}>
-                    <p>Shipping</p>
-                    <p >$$$   </p>
 
-                </Box>
+                    <Box display="flex" gap={60}   >
+                        <p>Shipping</p>
+                        <p >$$$   </p>
+
+                    </Box>
+                </Box >
                 <Divider />
                 <Divider />
 
-                <Box display="flex" alignItems="center" flexDirection="row" gap={1}>
+                <Box display="flex" alignItems="center" flexDirection="row" gap={0}>
 
-                    <Box display="flex" gap={119}>
+                    <Box display="flex" gap={64}>
                         <p>Total</p>
                         <p >$$$   </p>
 
                     </Box>
                 </Box>
-                <Divider/>
+                <Divider />
 
-                <box> </box>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 4, marginRight: 4 }}>
+                    <Button variant="outlined" style={{ marginRight: '14px' }}>Revised Order</Button>
+                    <Button variant="contained">Share & Save</Button>
+                </Box>
+
 
             </Box>
         </Container>
